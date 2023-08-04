cffaed5aebc1cae51b0f965cdcf29851c74e1ed2
Tamaratonyor Ebufegha <tammyebufegha@Tamaratonyors-MacBook-Air.local>
2023-08-04 13:29:45 -0600

    initial commit

---
+++ boards/simplyhired.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+class SimplyHired:
+    def scrape(url):
+        driver = webdriver.Chrome()
+        driver.get(url)
+        value = driver.find_elements_by_class_name('')
+        text = value.find_element_by_xpath('.') # include . if reading from an element as we do here with value
