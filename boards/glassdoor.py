87304042908bb2e2e0c20439aff05b46fea4f508
Tamaratonyor Ebufegha <tammyebufegha@Tamaratonyors-MacBook-Air.local>
2023-08-04 13:56:06 -0600

    glassdoor removal

--- boards/glassdoor.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-class Glassdoor:
-    def scrape(url):
-        driver = webdriver.Chrome()
-        driver.get(url)
-        value = driver.find_elements_by_class_name('')
-        text = value.find_element_by_xpath('.') # include . if reading from an element as we do here with value
